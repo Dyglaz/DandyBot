37c98af32c4cf5d439027c965ae16b28f0b4bc9d
Dyglaz <kallkae@mail.ru>
2023-02-21 22:32:07 +0300

    Game

--- user_bot.py
+++ user_bot.py
@@ -1,25 +1,30 @@
 ﻿def script(check, x, y):
     if check("gold", x, y):
         return "take"
-    return solve(check, x, y)
+    return movement(check, x, y)
 
-
-def solve(check, x, y):
-    path = find_gold_iter(check, x, y)
+def movement(check, x, y):
+    path = goldSearch(check, x, y)
     (x_, y_), (dx, dy) = path[:2]
-    direction = {(-1, 0): "left", (1, 0): "right", (0, -1): "up", (0, 1): "down"}
-    return direction[(dx - x_, dy - y_)]
+    match (dx - x_, dy - y_):
+        case (-1, 0):
+            return "left"
+        case (1, 0):
+            return "right"
+        case (0, -1):
+            return "up"
+        case (0, 1):
+            return "down"
 
-
-def find_gold_iter(check, x, y):
-    visited = {(x, y)}
-    queue = [[(x, y)]]
-    while queue:
-        path = queue.pop(0)
+def goldSearch(check, x, y):
+    visitedPath  = {(x, y)}
+    queuePath = [[(x, y)]]
+    while queuePath:
+        path = queuePath.pop(0)
         x_, y_ = path[-1]
         if check("gold", x_, y_):
             return path
         for dx, dy in [(x_ - 1, y_), (x_ + 1, y_), (x_, y_ - 1), (x_, y_ + 1)]:
-            if not check('wall', dx, dy) and (dx, dy) not in visited:
-                visited.add((dx, dy))
-                queue.append(path + [(dx, dy)])
+            if not check("wall", dx, dy) and (dx, dy) not in visitedPath:
+                visitedPath .add((dx, dy))
+                queuePath.append(path + [(dx, dy)])
